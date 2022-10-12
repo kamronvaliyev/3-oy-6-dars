d148d03264a9b81abac63495d827581736b37ca2
kamronvaliyev <kamrovaliyev0805@gmail.com>
2022-10-12 15:19:03 +0300

    ,

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
+//var dollor = 500 + 250;
+//var kurs1 = 11065.56 * dollor;
+//var kurs2 = 10731.16 * 120;
+//var kurs3 = kurs1 + kurs2;
 var elHeading = document.querySelector(".heading");
-var dollor = 500 + 250;
-var kurs1 = 11065.56 * dollor;
-var kurs2 = 10731.16 * 120;
-var kurs3 = kurs1 + kurs2;
+var money = 1000 + 500 + 300;
 alert("Xush kelibsiz");
-var yourMoney = Number(prompt("Sizning pulingiz qancha?"));
-if (yourMoney >= kurs3) {
-  elHeading.textContent = "Oq yo’l, Jonibek!";
+var yourMoney = Number(
+  prompt("Sizning pulingiz qancha? Pulingizni dollorda kiriting.")
+);
+if (yourMoney >= money) {
+  elHeading.textContent = "Barcha davlatlarga sahoyat qilish mumkin ";
+} else if (yourMoney >= 1000) {
+  elHeading.textContent = "Siz Dubaiga sahoyat qilish mumkin";
+} else if (yourMoney >= 500) {
+  elHeading.textContent = "Siz Rossiyaga sahoyat qilish mumkin";
+} else if (yourMoney >= 300) {
+  elHeading.textContent = "Siz Toshkentga sahoyat qilish mumkin";
 } else {
-  elHeading.textContent =
-    "Jonibek, Shahzoddan so`ra, balki berar! U ham bermasa bolalardan so`ra! Ular ham bermasa uyda telefonda kino ko`r.";
+  elHeading.textContent = "Uyda Dunyo Bo`ylab telekanali ko`rib yoting.";
 }
