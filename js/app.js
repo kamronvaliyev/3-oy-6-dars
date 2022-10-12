cae19599ec60f480b615225c3503762202c92c20
kamronvaliyev <kamrovaliyev0805@gmail.com>
2022-10-12 14:35:55 +0300

    .

--- js/app.js
+++ js/app.js
@@ -1,15 +1,10 @@
+var elHeading = document.querySelector(".heading");
 var dollor = 500 + 250;
-console.log(dollor);
 var kurs1 = 11065.56 * dollor;
-console.log(kurs1);
 var kurs2 = 10731.16 * 120;
-console.log(kurs2);
 var kurs3 = kurs1 + kurs2;
-console.log(kurs3);
 alert("Xush kelibsiz");
 var yourMoney = Number(prompt("Sizning pulingiz qancha?"));
-console.log(yourMoney);
-var elHeading = document.querySelector(".heading");
 if (yourMoney >= kurs3) {
   elHeading.textContent = "Oq yo’l, Jonibek!";
 } else {
